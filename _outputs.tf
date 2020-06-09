9877d557b3a2d13f2f19136c66c794e48068116a
vishal <vishal.bhogate07@gmail.com>
2020-06-09 12:25:23 +1000

    ECR repository, policy and lifecycle

--- _outputs.tf
+++ _outputs.tf
@@ -1,4 +1,4 @@
-output "name" {
-  value       = ""
-  description = ""
+output "ecrarn" {
+  value       = aws_ecr_repository.default.arn
+  description = "Full ARN of the repository"
 }
---
+++ ecr-lifecycle.tf
@@ -0,0 +1,54 @@
+resource aws_ecr_lifecycle_policy default {
+    count = var.required_ecr_lifecyle ? 1 : 0
+    repository = aws_ecr_repository.default.name
+    policy = <<EOF
+{
+    "rules": [
+       { 
+            "rulePriority": 1,
+            "description": "Expire images older than 14 days",
+            "selection": {
+                "countUnit": "days",
+                "countType": "sinceImagePushed",
+                "countNumber": 14,
+                "tagStatus": "untagged"
+            },
+            "action": {
+                "type": "expire"
+            }
+       },
+       {
+           "rulePriority": 2,
+           "description": "Expire images with feature tag",
+           "selection": 
+           {
+               "countType": "imageCountMoreThan",
+               "tagPrefixList": [
+                    "feature",
+                    "prod",
+                    "deploy",
+                    "qa"
+               ],
+               "countNumber": 1,
+               "tagStatus": "tagged"
+           },
+           "action": {
+               "type": "expire"
+           }
+       },
+       {
+           "rulePriority": 3,
+           "description": "Expire images with the same tag",
+           "selection": {
+               "countType": "imageCountMoreThan",
+               "countNumber": 1,
+               "tagStatus": "any"
+           },
+           "action": {
+               "type": "expire"
+           }
+       }
+    ]
+}
+EOF
+}
